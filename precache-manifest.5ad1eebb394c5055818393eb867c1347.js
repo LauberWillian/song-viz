89febea664e47778a6c9af81b0bd35184659f850
Willian Alberto Lauber <wlauber@pm.me>
2022-02-27 12:21:47 -0300

    Updates

---
+++ precache-manifest.5ad1eebb394c5055818393eb867c1347.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a258fe9432ef186d946bffb9b30b8f4c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b24994f24434b3e4fafd",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "418fe4f800a881d7bc03",
+    "url": "./static/js/2.795276d1.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "./static/js/2.795276d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b24994f24434b3e4fafd",
+    "url": "./static/js/main.a321965e.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.a321965e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1b6eebf95e7304837a",
+    "url": "./static/js/runtime-main.373e0700.js"
+  },
+  {
+    "revision": "3fddc9a84224d520dec2957932aa67f2",
+    "url": "./static/media/IZECOLD - Close (feat. Molly Ann) [NCS Release].3fddc9a8.mp3"
+  },
+  {
+    "revision": "24df85141f89b178a620b7e428369da2",
+    "url": "./static/media/close brooks remix cover.24df8514.jpg"
+  }
+]);
